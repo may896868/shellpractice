0dc87a868bbd75bf231c81e7e875983a909fb7fa
may896868 <may896868@gmail.com>
2025-09-25 21:10:54 +0530

    installation commit

--- installation.sh
+++ installation.sh
@@ -15,3 +15,21 @@ if [ $? -ne 0 ]; then
 else
     echo "installing mysql is success"
 fi
+
+dnf install nginx -y
+
+if [ $? -ne 0 ]; then
+    echo "Error : installing nginx is failed"
+    exit 1
+else
+    echo "installing mysql is success"
+fi
+
+dnf install python3 -y
+
+if [ $? -ne 0 ]; then
+    echo "Error : installing python3 is failed"
+    exit 1
+else
+    echo "installing python3 is success"
+fi
