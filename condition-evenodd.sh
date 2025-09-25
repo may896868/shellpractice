2d49e249ad78a3bb221b1e91fdd7df01063613b4
may896868 <may896868@gmail.com>
2025-09-25 17:22:13 +0530

    condition-evenodd commmit

---
+++ condition-evenodd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "enter the number
+read NUMBER
+
+if [ $((NUMBER % 2)) -eq 0 ]; then
+    echo "the given number $NUMBER is even number"
+else 
+    echo "the iven number $NUMBER is odd number"
