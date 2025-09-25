169ec124ef53dd2f6b14f8c8851ee17634208bc4
may896868 <may896868@gmail.com>
2025-09-25 12:54:21 +0530

    special var commit

---
+++ special-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "All variables passed to the script : $@"
+echo "All variables passed to the script : $*"
+echo "script name : $0"
+echo "current directory : $PWD"
+echo "User name : $ USER"
+echo "user home dir : $HOME"
+echo "pid of thi script : $$"
+
+sleep 50 &
+
+echo "pid of the last command in background : $!"
