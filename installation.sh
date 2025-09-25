cd8eda6a57915d7ba333a8d128fdbbf32ab82614
may896868 <may896868@gmail.com>
2025-09-25 18:27:49 +0530

    installation commmit

--- installation.sh
+++ installation.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR : PLEASE RUN THIS SCRIPT  WITH root privileges"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "Error : installing mysql is failed"
+    exit 1
 else
     echo "installing mysql is success"
 fi
