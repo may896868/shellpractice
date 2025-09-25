5150185c7b0c93432a0dd0c833a25cbf3db0eae6
may896868 <may896868@gmail.com>
2025-09-25 21:14:59 +0530

    clours-1 commit

---
+++ colours-1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR : PLEASE RUN THIS SCRIPT  WITH root privileges"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo " installing $2 is $R FAILED $N"
+        exit 1
+    else
+        echo "installing $2 is $R SUCCESS $N"
+    fi
+}
+
+dnf list installed mysql
+if ($? -ne 0); then
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e "my sql already exist $Y SKIPPING $N"
+fi
+
+dnf list installed nginx
+if ($? -ne 0); then
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "nginx already exist $Y SKIPPING $N"
+fi
+
+dnf list installed python3
+if ($? -ne 0); then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+    else
+    echo -e "python already exist $Y SKIPPING $N"
+fi
