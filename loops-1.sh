909f829328a587ddd1412934f4886f7abf39db69
may896868 <may896868@gmail.com>
2025-09-27 21:52:30 +0530

    loops-1 commit

--- loops-1.sh
+++ loops-1.sh
@@ -31,5 +31,14 @@ VALIDATE(){
 
 for package in $@
 do
-    echo "package is : $package"
+    # check the package is already installed or not
+    dnf list installed $package &>>$LOG_FILE
+
+    # if exit status -eq 0 already installed, -ne 0 need to install
+    if [ $? -ne 0 ]; then
+        dnf install $package -y  &>>$LOG_FILE
+        VALIDATE $? "$package"
+    else 
+        echo -e "$package is already installed ... $Y SKIPPED $N"
+    fi
 done
