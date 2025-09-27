1de3977e9e1c4b957c42205970fd1de84e33d067
may896868 <may896868@gmail.com>
2025-09-27 16:08:18 +0530

    logs commit

---
+++ logs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shesllscript"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo " script started executed at $(date)"
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR : PLEASE RUN THIS SCRIPT  WITH root privileges"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo " installing $2 is $R FAILED $N"
+        exit 1
+    else
+        echo "installing $2 is $R SUCCESS $N"
+    fi
+}
+
+dnf list installed mysql &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install mysql -y &>>$LOG_FILE
+    VALIDATE $? "mysql"
+else
+    echo -e "my sql already exist $Y SKIPPING $N"
+fi
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATE $? "nginx"
+else
+    echo -e "nginx already exist $Y SKIPPING $N"
+fi
+
+dnf list installed python3 &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    dnf install python3 -y &>>$LOG_FILE
+    VALIDATE $? "python3"
+    else
+    echo -e "python3 already exist $Y SKIPPING $N"
+fi
